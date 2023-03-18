4c135bc348f9d4a8af3a93b1120a4c6aefdc5f02
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-19 01:50:37 +0600

    header and footer added

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,20 +1,26 @@
+import Logo from '../../assets/INOV.jpg'
+import { Link } from 'react-router-dom'
+import { Search } from '../Sections/Search'
 export const Header = () => {
   return (
     <header>
     
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-        <a href="https://flowbite.com" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
+        <Link to="/" className="flex items-center">
+            <img src={Logo} className="h-6 mr-3 sm:h-9" alt="InovBook Store" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Store</span>
+        </Link>
         <div className="flex items-center">
-            <a href="tel:5541251234" className="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" className="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+            <span className='class="bi bi-gear-wide mr-5 text-xl text-gray-500 dark:text-white cursor-pointer'></span>
+            <span className="bi bi-search mr-5 text-xl text-gray-500 dark:text-white cursor-pointer"></span>
+            <span className="bi bi-bag mr-5 text-xl text-gray-500 dark:text-white cursor-pointer"></span>
+            <span className='bi bi-person-circle mr-5 text-xl text-gray-500 dark:text-white cursor-pointer'></span>
+           
         </div>
     </div>
 </nav>
-
+<Search/>
 
     </header>
   )
