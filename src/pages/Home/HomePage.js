818c49638641d967a7405078f3e8e9063eafd295
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-25 17:01:39 +0600

    home page update

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,10 +1,12 @@
 import HeroSec from './components/HeroSec'
 import FeaturedProduct from './components/FeaturedProduct'
+import Testimonial from './components/Testimonial'
 export const HomePage = () => {
   return (
     <main>
         <HeroSec/>
         <FeaturedProduct/>
+        <Testimonial/>
     </main>
   )
 }
--- src/pages/Home/components/FeaturedProduct.js
+++ src/pages/Home/components/FeaturedProduct.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ProductCard from '../../../components/Elements/ProductCard'
 
 const FeaturedProduct = () => {
   return (
-    <div className="text-center text-3xl font-medium mt-10">FeaturedProduct</div>
+    <selection className="my-20">
+        <h2 className="text-center text-3xl font-medium mt-10">FeaturedProduct</h2>
+        <div className=" flex flex-wrap justify-center lg:flex-row" >
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+    </selection>
   )
 }
 
 export default FeaturedProduct
+ 
