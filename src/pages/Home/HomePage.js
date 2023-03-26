2fb661d3dfceb1105ae176058c57d0e934f4ccb0
sakibsarker <sakib15-3824@diu.edu.bd>
2023-03-26 20:00:52 +0600

    faq page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,12 +1,14 @@
 import HeroSec from './components/HeroSec'
 import FeaturedProduct from './components/FeaturedProduct'
 import Testimonial from './components/Testimonial'
+import Faq from './components/Faq'
 export const HomePage = () => {
   return (
     <main>
         <HeroSec/>
         <FeaturedProduct/>
         <Testimonial/>
+        <Faq/>
     </main>
   )
 }
