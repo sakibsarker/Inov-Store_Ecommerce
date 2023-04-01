d77deb371497b1a11aa83c8318146bad8938935c
sakibsarker <sakib15-3824@diu.edu.bd>
2023-04-02 01:32:59 +0600

    json and product list

--- src/pages/Home/components/FeaturedProduct.js
+++ src/pages/Home/components/FeaturedProduct.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from '../../../components/Elements/ProductCard'
 
 const FeaturedProduct = () => {
+  const [products,setProduct]=useState([]);
+  useEffect(()=>{
+    async function fetchproduct(){
+      const res=await fetch("http://localhost:8000/products")
+      const data=await res.json()
+      setProduct(data)
+    }
+    fetchproduct()
+  },[])
   return (
     <selection className="my-20">
         <h2 className="text-center text-3xl font-medium mt-10">FeaturedProduct</h2>
         <div className=" flex flex-wrap justify-center lg:flex-row" >
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
+          {
+            products.map((product)=>(
+              <ProductCard key={product.id} product={product}/>
+            ))
+          }
+         
+  
         </div>
     </selection>
   )
